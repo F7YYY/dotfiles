f6c4e4cd0c3dfd236afc9f4dc0b5a9a0d6b94096
F7YYY <38305613+F7YYY@users.noreply.github.com>
2024-03-28 02:08:17 -0500

    Prettier

--- .config/hypr/scripts/battery.sh
+++ .config/hypr/scripts/battery.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
+#
+# hyprlock plugin
+#################
+#
+# Main function to verify battery & then report status  
+main() {
+    enable_battery=false
+    battery_charging=false
 
-############ Variables ############
-enable_battery=false
-battery_charging=false
-
-####### Check availability ########
-for battery in /sys/class/power_supply/*BAT*; do
-    if [[ -f "$battery/uevent" ]]; then
-        enable_battery=true
-        if [[ $(cat /sys/class/power_supply/*/status | head -1) == "Charging" ]]; then
-            battery_charging=true
+    # Verify battery is available & it's state
+    for battery in /sys/class/power_supply/*BAT*; do
+        if [[ -f "$battery/uevent" ]]; then
+            enable_battery=true
+            if [[ $(cat /sys/class/power_supply/*/status | head -1) == "Charging" ]]; then
+                battery_charging=true
+            fi
+            break
         fi
-        break
-    fi
-done
+    done
 
-############# Output #############
-if [[ $enable_battery == true ]]; then
-    if [[ $battery_charging == true ]]; then
-        echo -n "(+) "
+    # Report the current battery status
+    if [[ $enable_battery == true ]]; then
+        if [[ $battery_charging == true ]]; then
+            echo -n "(+) "
+        fi
+        echo -n "$(cat /sys/class/power_supply/*/capacity | head -1)"%
+        if [[ $battery_charging == false ]]; then
+            echo -n " remaining"
+        fi
     fi
-    echo -n "$(cat /sys/class/power_supply/*/capacity | head -1)"%
-    if [[ $battery_charging == false ]]; then
-        echo -n " remaining"
-    fi
-fi
 
-echo ''
+    echo ''
+}
+
+# Start of Script
+main
