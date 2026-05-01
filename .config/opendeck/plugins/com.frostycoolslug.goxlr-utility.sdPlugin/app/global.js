485d55488cd13a53b0fa14ebd0764987c911c92a
F7YYY <38305613+F7YYY@users.noreply.github.com>
2026-05-01 18:16:59 -0500

    UPDATE

--- .config/opendeck/plugins/com.frostycoolslug.goxlr-utility.sdPlugin/app/global.js
+++
@@ -1,8 +0,0 @@
-// The Event Target for Patch Updates
-const eventTarget = new EventTarget();
-
-// The websocket for communciating with the Utility
-const websocket = new Websocket();
-
-// The current 'DaemonStatus'.
-let status = undefined;
--- .config/opendeck/plugins/me.amankhanna.oadesktopentry.sdPlugin/pi/_app/immutable/chunks/Cy2ArZ8V.js
+++
@@ -1,1 +0,0 @@
-import{u as o,f as t,l as c,g as l}from"./BnK9gjKz.js";function u(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function i(e){t===null&&u(),c&&t.l!==null?a(t).m.push(e):o(()=>{const n=l(e);if(typeof n=="function")return n})}function a(e){var n=e.l;return n.u??={a:[],b:[],m:[]}}export{i as o};
