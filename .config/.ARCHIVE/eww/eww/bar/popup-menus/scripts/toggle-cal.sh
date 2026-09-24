11ce39ae0b1c063bbdca3251ad0a3c5ad45635ac
F7YYY <38305613+F7YYY@users.noreply.github.com>
2026-09-23 19:56:37 -0500

    UPDATE

--- .config/.ARCHIVE/eww/eww/bar/popup-menus/scripts/toggle-cal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -z $(eww active-windows | grep 'calender') ]]; then
-    eww open calender && eww update calrev=true
-else
-    eww update calrev=false
-    (sleep 0.2 && eww close calender) &
-fi
